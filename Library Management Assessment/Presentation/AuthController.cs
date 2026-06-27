96267f21e1bb72db9f23286b798150c5ecbebcd0
ziadali007 <ziadali007@users.noreply.github.com>
2026-06-27 17:42:38 +0300

    Member Registration

---
+++ Library Management Assessment/Presentation/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Services.Abstractions;
+using Shared.Register_LoginDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class AuthController(IServiceManager serviceManager) : ControllerBase
+    {
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto dto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var (success, errors) = await serviceManager.AuthService.RegisterMemberAsync(dto);
+
+            if (!success)
+            {
+                return BadRequest(new { message = "Registration failed", errors });
+            }
+
+            return Ok(new { message = "User registered successfully as a Member." });
+        }
+
+    }
+}
---
+++ Library Management Assessment/Services.Abstractions/IAuthService.cs
@@ -0,0 +1,14 @@
+﻿using Shared.Register_LoginDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Abstractions
+{
+    public interface IAuthService
+    {
+        Task<(bool Success, IEnumerable<string> Errors)> RegisterMemberAsync(RegisterUserDto dto);
+    }
+}
